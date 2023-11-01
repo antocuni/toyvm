3c974945d68e7a03d67a93ef0beae28dff5421d5
Antonio Cuni <anto.cuni@gmail.com>
2023-11-01 16:13:24 +0100

    move tests into a class

--- toyvm/tests/test_frame.py
+++ toyvm/tests/test_frame.py
@@ -2,57 +2,59 @@ from toyvm.frame import Frame
 from toyvm.opcode import OpCode, CodeObject
 from toyvm.objects import W_Int, W_Str
 
-def test_simple():
-    code = CodeObject([
-        OpCode('load_const', W_Int(2)),
-        OpCode('load_const', W_Int(4)),
-        OpCode('add'),
-        OpCode('return')
-    ])
-    frame = Frame(code)
-    w_res = frame.run()
-    assert w_res == W_Int(6)
+class TestFrame:
 
-def test_locals():
-    code = CodeObject([
-        OpCode('load_const', W_Int(2)),
-        OpCode('store_local', 'a'),
-        OpCode('load_local', 'a'),
-        OpCode('return')
-    ])
-    frame = Frame(code)
-    w_res = frame.run()
-    assert w_res == W_Int(2)
+    def test_simple(self):
+        code = CodeObject([
+            OpCode('load_const', W_Int(2)),
+            OpCode('load_const', W_Int(4)),
+            OpCode('add'),
+            OpCode('return')
+        ])
+        frame = Frame(code)
+        w_res = frame.run()
+        assert w_res == W_Int(6)
 
-def test_add_str():
-    code = CodeObject([
-        OpCode('load_const', W_Str('hello ')),
-        OpCode('load_const', W_Str('world')),
-        OpCode('add'),
-        OpCode('return')
-    ])
-    frame = Frame(code)
-    w_res = frame.run()
-    assert w_res == W_Str('hello world')
+    def test_locals(self):
+        code = CodeObject([
+            OpCode('load_const', W_Int(2)),
+            OpCode('store_local', 'a'),
+            OpCode('load_local', 'a'),
+            OpCode('return')
+        ])
+        frame = Frame(code)
+        w_res = frame.run()
+        assert w_res == W_Int(2)
 
-def test_mul_int():
-    code = CodeObject([
-        OpCode('load_const', W_Int(2)),
-        OpCode('load_const', W_Int(4)),
-        OpCode('mul'),
-        OpCode('return')
-    ])
-    frame = Frame(code)
-    w_res = frame.run()
-    assert w_res == W_Int(8)
+    def test_add_str(self):
+        code = CodeObject([
+            OpCode('load_const', W_Str('hello ')),
+            OpCode('load_const', W_Str('world')),
+            OpCode('add'),
+            OpCode('return')
+        ])
+        frame = Frame(code)
+        w_res = frame.run()
+        assert w_res == W_Str('hello world')
 
-def test_mul_str():
-    code = CodeObject([
-        OpCode('load_const', W_Str('x')),
-        OpCode('load_const', W_Int(4)),
-        OpCode('mul'),
-        OpCode('return')
-    ])
-    frame = Frame(code)
-    w_res = frame.run()
-    assert w_res == W_Str('xxxx')
+    def test_mul_int(self):
+        code = CodeObject([
+            OpCode('load_const', W_Int(2)),
+            OpCode('load_const', W_Int(4)),
+            OpCode('mul'),
+            OpCode('return')
+        ])
+        frame = Frame(code)
+        w_res = frame.run()
+        assert w_res == W_Int(8)
+
+    def test_mul_str(self):
+        code = CodeObject([
+            OpCode('load_const', W_Str('x')),
+            OpCode('load_const', W_Int(4)),
+            OpCode('mul'),
+            OpCode('return')
+        ])
+        frame = Frame(code)
+        w_res = frame.run()
+        assert w_res == W_Str('xxxx')
