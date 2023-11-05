990972154fa69b631ff4d37e086721b3cdbae2f7
Antonio Cuni <anto.cuni@gmail.com>
2023-11-05 17:42:02 +0100

    add more tests for pc remapping

--- toyvm/tests/test_rainbow.py
+++ toyvm/tests/test_rainbow.py
@@ -1,9 +1,14 @@
-from toyvm.rainbow import peval
+from toyvm.rainbow import RainbowInterpreter
 from toyvm.opcode import OpCode, CodeObject
 from toyvm.objects import W_Int, W_Str, W_Function
 from toyvm.compiler import toy_compile
 
 class TestRainbow:
+
+    def peval(self, code):
+        self.interp = RainbowInterpreter(code)
+        self.interp.run()
+        return self.interp.out
 
     def test_simple(self):
         code = CodeObject('fn', [
@@ -12,7 +17,7 @@ class TestRainbow:
             OpCode('add'),
             OpCode('return'),
         ])
-        code2 = peval(code)
+        code2 = self.peval(code)
         assert code2.body == code.body
 
     def test_green_op(self):
@@ -22,7 +27,7 @@ class TestRainbow:
             OpCode('add'),
             OpCode('return'),
         ])
-        code2 = peval(code)
+        code2 = self.peval(code)
         assert code2.body == [
             OpCode('load_const', W_Int(3)),
             OpCode('return'),
@@ -37,7 +42,7 @@ class TestRainbow:
             OpCode('add'),
             OpCode('return'),
         ])
-        code2 = peval(code)
+        code2 = self.peval(code)
         assert code2.body == [
             OpCode('load_local', 'a'),
             OpCode('load_const', W_Int(6)),
@@ -54,7 +59,7 @@ class TestRainbow:
             OpCode('mul'),
             OpCode('return'),
         ])
-        code2 = peval(code)
+        code2 = self.peval(code)
         assert code2.body == code.body
 
     def test_green_br_if(self):
@@ -67,7 +72,7 @@ class TestRainbow:
             OpCode('return'),                # 5
             OpCode('abort', 'unreachable')   # 6 "endif"
         ])
-        code2 = peval(code)
+        code2 = self.peval(code)
         assert code2.body == [
             OpCode('load_const', W_Int(2)),
             OpCode('return'),
@@ -84,10 +89,71 @@ class TestRainbow:
             OpCode('return'),                # 5
             OpCode('abort', 'unreachable')   # 6 "endif"
         ])
-        code2 = peval(code)
+        code2 = self.peval(code)
         assert code2.body == code.body
 
-    def test_pc_remapping(self):
+    def test_pc_remap_red(self):
+        code = CodeObject('fn', [
+            OpCode('load_local', 'a'),       # 0
+            OpCode('load_local', 'b'),       # 1
+            OpCode('add'),                   # 2
+        ])
+        code2 = self.peval(code)
+        assert code.body == code2.body
+        assert self.interp.pcmap == {
+            0: 0,
+            1: 1,
+            2: 2,
+        }
+
+    def test_pc_remap_flush1(self):
+        code = CodeObject('fn', [
+            OpCode('load_const', W_Int(1)),  # 0
+            OpCode('load_local', 'b'),       # 1
+            OpCode('add'),                   # 2
+        ])
+        code2 = self.peval(code)
+        assert code.body == code2.body
+        assert self.interp.pcmap == {
+            0: 0,
+            1: 1,
+            2: 2,
+        }
+
+    def test_pc_remap_flush2(self):
+        code = CodeObject('fn', [
+            OpCode('load_local', 'a'),       # 0
+            OpCode('load_const', W_Int(1)),  # 1
+            OpCode('add'),                   # 2
+        ])
+        code2 = self.peval(code)
+        assert code.body == code2.body
+        assert self.interp.pcmap == {
+            0: 0,
+            1: 1,
+            2: 2,
+        }
+
+    def test_pc_remap_green(self):
+        code = CodeObject('fn', [
+            OpCode('load_const', W_Int(2)),  # 0
+            OpCode('load_const', W_Int(1)),  # 1
+            OpCode('add'),                   # 2
+            OpCode('return'),                # 3
+        ])
+        code2 = self.peval(code)
+        assert code2.body == [
+            OpCode('load_const', W_Int(3)),  # 0
+            OpCode('return'),                # 1
+        ]
+        assert self.interp.pcmap == {
+            0: 0,
+            1: 0,
+            2: 0,
+            3: 1,
+        }
+
+    def test_pc_remap_branch(self):
         code = CodeObject('fn', [
             OpCode('load_local', 'a'),       # 0
             OpCode('br_if', 2, 6, 8),        # 1
@@ -99,7 +165,7 @@ class TestRainbow:
             OpCode('return'),                # 7
             OpCode('abort', 'unreachable')   # 8
         ])
-        code2 = peval(code)
+        code2 = self.peval(code)
         assert code2.body == [
             OpCode('load_local', 'a'),       # 0
             OpCode('br_if', 2, 4, 6),        # 1
