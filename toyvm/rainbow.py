6ae12ad11c4b739c5e62a5f5bcf58aadb04878c8
Antonio Cuni <anto.cuni@gmail.com>
2023-11-06 22:34:31 +0100

    remove an hardcoded value

--- toyvm/rainbow.py
+++ toyvm/rainbow.py
@@ -59,7 +59,7 @@ class RainbowInterpreter:
     def run_single_op(self, pc):
         op = self.code.body[pc].copy()
         meth = getattr(self, f'op_{op.name}', self.op_default)
-        pc_next = meth(op, *op.args)
+        pc_next = meth(pc, op, *op.args)
         if pc_next is None:
             return pc + 1
         else:
@@ -95,16 +95,16 @@ class RainbowInterpreter:
         pops = op.num_pops()
         return self.n_greens() >= pops
 
-    def op_default(self, op, *args):
+    def op_default(self, pc, op, *args):
         if self.is_green(op):
-            return self.op_green(op, *args)
+            return self.op_green(pc, op, *args)
         else:
-            return self.op_red(op, *args)
+            return self.op_red(pc, op, *args)
 
-    def op_green(self, op, *args):
+    def op_green(self, pc, op, *args):
         self.greenframe.run_op(op)
 
-    def op_red(self, op, *args):
+    def op_red(self, pc, op, *args):
         self.flush()
         pops = op.num_pops()
         assert self.stack_length >= pops # sanity check
@@ -112,14 +112,14 @@ class RainbowInterpreter:
         self.stack_length += op.num_pushes()
         self.emit(op)
 
-    def op_load_local_green(self, op, varname):
-        return self.op_green(op, varname)
+    def op_load_local_green(self, pc, op, varname):
+        return self.op_green(pc, op, varname)
 
-    def op_store_local_green(self, op, varname):
+    def op_store_local_green(self, pc, op, varname):
         assert self.n_greens() >= 1, 'store_local_green called on a red'
-        return self.op_green(op, varname)
+        return self.op_green(pc, op, varname)
 
-    def op_br_if(self, op, then_pc, else_pc, endif_pc):
+    def op_br_if(self, pc, op, then_pc, else_pc, endif_pc):
         if self.n_greens() >= 1:
             # green case
             w_cond = self.greenframe.stack.pop()
@@ -129,7 +129,7 @@ class RainbowInterpreter:
                 self.run_range(else_pc, endif_pc)
             return endif_pc
         else:
-            self.op_red(op, *op.args) # emit br_if
+            self.op_red(pc, op, *op.args) # emit br_if
             self.record_jump_target(then_pc)
             #
             self.run_range(then_pc, else_pc)
@@ -140,33 +140,31 @@ class RainbowInterpreter:
             #
             return endif_pc
 
-    def op_get_iter(self, op, itername):
+    def op_get_iter(self, pc, op, itername):
         is_red = self.n_greens() < 1 or not self.greenframe.stack[-1].unroll
         if is_red:
-            return self.op_red(op, itername)
+            return self.op_red(pc, op, itername)
         else:
-            return self.op_green(op, itername)
+            return self.op_green(pc, op, itername)
 
-    def op_for_iter(self, op, itername, targetname, endfor_pc):
+    def op_for_iter(self, pc, op, itername, targetname, endfor_pc):
         w_iter = self.greenframe.locals.get(itername)
         if w_iter is None:
             # this is a non-unrolling for
-            # XXX implement me
             import pdb;pdb.set_trace()
         else:
-            return self.op_unroll_for_iter(op, itername, targetname, endfor_pc,
-                                           w_iter)
+            return self.op_unroll_for_iter(pc, op, itername, targetname,
+                                           endfor_pc, w_iter)
 
-    def op_unroll_for_iter(self, op, itername, targetname, endfor_pc, w_iter):
+    def op_unroll_for_iter(self, pc, op, itername, targetname,
+                           endfor_pc, w_iter):
         assert w_iter.unroll
         assert targetname.isupper()
-        PC = 5 # XXX this is hardcoded
-        #
         br_pc = endfor_pc - 1
         assert self.code.body[br_pc].name == 'br' # op to loop back
         #
         for w_item in w_iter._iter:
             self.greenframe.locals[targetname] = w_item
-            self.run_range(PC+1, br_pc)
-
+            self.run_range(pc+1, br_pc)
+        #
         return endfor_pc
